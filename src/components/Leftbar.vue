ac2a8484d6f38f67df32bba08ebb2c16aa8ebd82
yuan <584514950@qq.com>
2017-08-14 20:36:28 +0800

    add leftbar

--- src/components/Leftbar.vue
+++ src/components/Leftbar.vue
@@ -1,19 +1,56 @@
 <template>
   <div id="leftbar">
-    leftbar
+    <nav>
+      <ol>
+        <li v-for="(item,index) in resume.visibleItems" :class="{active: item === selected}" @click="selected = item">{{index}}</li>
+      </ol>
+    </nav>
+    <ol class="panels">
+      <li v-for="item in resume.visibleItems" v-show="item === selected">{{resume[item]}}</li>
+    </ol>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Leftbar'
+  name: 'Leftbar',
+  data(){
+    return {
+      selected: 'bio',
+      resume: {
+        visibleItems: ['bio','work history','education','projects','awards','contacts','others'],
+        bio: {
+          name: '',
+          city: '',
+          title: ''
+        }
+      }
+    }
+  }
 }
 
 </script>
 
-<style>
+<style lang="scss">
   #leftbar {
-    color: #fff;
-    width: 60px;
+    display: flex;
+    flex-direction: row;
+    > nav {
+      color: #fff;
+      width: 50px;
+      > ol > li{
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        &.active{
+          background: rgb(54,185,183);
+          color: #fff;
+        }
+      }
+    }
+    .panels {
+      overflow: auto;
+    }
   }
 </style>
