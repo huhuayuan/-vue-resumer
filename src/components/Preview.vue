3a029b5be4fbf17c4e5bb8d7441a2b432b64a52d
yuan <584514950@qq.com>
2017-08-17 10:50:57 +0800

    add preview content

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,6 +1,63 @@
 <template>
   <div id="preview">
-    {{resume}}
+    <section data-name="profile" v-show="resume.profile">
+      <h1>{{resume.profile.name}}</h1>
+      <h2>{{resume.profile.title}}</h2>
+      <p>
+        <small>{{resume.profile.city}}</small>
+        <!-- <small>{{resume.profile.birthday}}</small> -->
+      </p>
+    </section>
+    <section data-name="projects" v-show="resume.projects">
+      <h2>项目经历</h2>
+      <ol>
+        <li v-for="item in resume.projects">
+          <h3>{{item.name}}
+            <span v-show="item.content"> - {{item.content}}</span>
+          </h3>
+        </li>
+      </ol>
+    </section>
+    <section data-name="workHistory" v-show="resume.workHistory">
+      <h2>工作经历</h2>
+      <ol>
+        <li v-for="item in resume.workHistory">
+          <h3>{{item.company}}</h3>
+          <p v-show="item.content">{{item.content}}</p>
+        </li>
+      </ol>
+    </section>
+    <section data-name="education" v-show="resume.education">
+      <h2>教育经历</h2>
+      <ol>
+        <li v-for="item in resume.education">
+          <h3>{{item.school}}
+            <span v-show="item.content"> - {{item.content}}</span>
+          </h3>
+        </li>
+      </ol>
+    </section>
+    <section data-name="awards" v-show="resume.awards">
+      <h2>获奖情况</h2>
+      <ol>
+        <li v-for="item in resume.awards">
+          <h3>{{item.name}}
+            <span v-show="item.content"> - {{item.content}}</span>
+          </h3>
+        </li>
+      </ol>
+    </section>
+    <section data-name="projects" v-show="resume.contacts">
+      <h2>联系方式</h2>
+      <ol>
+        <li v-for="item in resume.contacts">
+          <p>
+            <span>{{item.contact}}</span>
+            <span v-show="item.content"> - {{item.content}}</span>
+          </p>
+        </li>
+      </ol>
+    </section>
   </div>
 </template>
 
@@ -11,14 +68,71 @@
       resume(){
         return this.$store.state.resume
       }
+    },
+    created() {
+      console.log(this.resume)
     }
   }
 </script>
 
-<style>
+<style lang="scss">
   #preview {
     flex: 1;
     background: #fff;
     box-shadow: 0 1px 3px 0 rgba(0,0,0,.2);
+    padding: 16px;
+    color: #333;
+    line-height: 1.2;
+    overflow: auto;
+    * {
+      padding: 0;
+      margin: 0;
+      font-weight: normal;
+    }
+    section + section {
+      margin-top: 2em;
+    }
+    p {
+      white-space: pre-line;
+    }
+    section {
+      > h2:first-child {
+        background-color: #ddd;
+        display: inline-block;
+        padding: .2em;
+        margin-bottom: .5em;
+      }
+    }
+    section[data-name="profile"]{
+      > h1 {
+        margin: .1em 0;
+        font-size: 2em;
+      }
+    }
+    section[data-name="workHistory"],
+    section[data-name="projects"],
+    section[data-name="awards"]
+    {
+      li + li {
+        margin-top: 1em;
+      }
+      li {
+        h3 {
+          border-bottom: 1px dashed #999;
+          padding-bottom: .3em;
+          margin-bottom: .3em;
+        }
+      }
+    }
+    section[data-name="education"]{
+      li {
+        line-height: 1.5em;
+      }
+    }
+    section[data-name="contacts"]{
+      li {
+        line-height: 1.5em;
+      }
+    }
   }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -8,32 +8,37 @@ export default new Vuex.Store({
     resume: {
       config: [
         {field: 'profile',icon:'id'},
-        {field: 'work history',icon:'work'},
+        {field: 'workHistory',icon:'work'},
         {field: 'education',icon:'book'},
         {field: 'projects',icon:'heart'},
         {field: 'awards',icon:'cup'},
         {field: 'contacts',icon:'phone'}
       ],
       profile: {
-        name: '',
-        city: '',
-        title: ''
+        name: '狐某某',
+        city: '北京',
+        title: '前端开发工程师',
+        birthday: '1991-10-24'
       },
-      'work history': [
-        {company: 'AL',content: '我的第二份工作'},
-        {company: 'XL',content: '我的第一份工作'}
+      'workHistory': [
+        {company: '北京百泉众合数据科技有限公司',content: `公司主做日志分析的。
+        我的主要工作如下：
+        1.完成产品需求'。
+        2.修复bug。`
+      },
+        {company: '北京建迅教育科技有限公司',content: '我的第一份工作'}
       ],
       education: [
-        {school: 'AL',content: '**学校'},
-        {school: 'XL',content: '**学校'}
+        {school: '山西电力学院',content: '计算机信息安全技术'},
+        {school: '洪洞一中'}
       ],
       projects: [
         {name: 'projectA',content: '文字'},
         {name: 'projectB',content: '文字'}
       ],
       awards: [
-        {name: 'awardsA',content: '文字'},
-        {name: 'awardsB',content: '文字'}
+        {name: '国家励志奖学金',content: '在校期间获国家励志奖学金'},
+        {name: '学期二等奖',content: '在校期间获学校二等奖'}
       ],
       contacts: [
         {contact: 'phone',content: '15513571357'},
